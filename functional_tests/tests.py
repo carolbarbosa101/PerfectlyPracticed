037816e4a444a32bc5a0bd4131a779a4a230926e
Anwin Robin <axr005@student.bham.ac.uk>
2021-07-01 14:23:02 +0100

    Created app, created initial functional and unit tests for login page

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,6 +1,35 @@
 from selenium import webdriver
+from django.test import LiveServerTestCase
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(LiveServerTestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  
+        self.browser.quit()
+
+    # Ziggy has heard about this cool new website to help with his guitar practice
+    # He opens his browser and goes to link
+    def test_open_site_and_check_title(self):  
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Music Hub', self.browser.title)  
+        
+        self.fail('Finish the test!')  
+
+    # He is presented with a login page with username and password textboxes
+    # Ziggy does not have an account so he chooses the sign up option that is below the entry fields
+
+    # He enters his details into the boxes and clicks the register button
+    def test_registration(self):
+        pass
+
+    # He is redirected to the login page again now 
+    # Ziggy enters his details and sucessfully logs in
+    def test_login(self):
+        pass
+
+    # He is now re-directed to his dashboard
+
+
+    
---
+++ musicapp/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+# Create your tests here.
+class LoginPageTest(TestCase):
+
+    def test_uses_login_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_sign_up_link(self):
+        pass
+
+    def test_uses_sign_up_template(self):
+        pass
+
+    def test_redirect_to_login(self):
+        pass
+
+    def test_redirect_to_dashboard(self):
+        pass
