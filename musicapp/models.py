a4ded72125776317b4796d30919ac3cfd269c97e
Anwin Robin <axr005@student.bham.ac.uk>
2021-07-05 14:02:36 +0100

    DB changed from sqlite3 to PostgreSQL

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    pass
+
+class User(AbstractBaseUser):
+    pass
+    
