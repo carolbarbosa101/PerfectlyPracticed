fd8f617857af5d85af65d7a5ae5753596e76b22a
Anwin Robin <axr005@student.bham.ac.uk>
2021-07-14 14:10:58 +0100

    Goals model created, view modified to handle goal post, template modified to accept and display goals

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+class Goals(models.Model):
+    text = models.TextField(default='')
+    due_date = models.DateField()
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from dashboard.models import Goals
+from django.shortcuts import redirect, render
 import datetime 
 from calendar import HTMLCalendar
+from dashboard.my_calendar import CustomCal
 
 
 def dashboard(request):
+    if request.method == 'POST':
+        Goals.objects.create(text=request.POST['goal_input'], due_date=request.POST['date_input'])
+        return redirect('dashboard')
+    goals = Goals.objects.all()
     today = datetime.datetime.today()
-    cal = HTMLCalendar().formatmonth(today.year, today.month)
-    return render(request, 'dashboard/dashboard.html',{'cal':cal})
+    cal = CustomCal().formatmonth(today.year, today.month)
+    return render(request, 'dashboard/dashboard.html',{'goals':goals, 'cal':cal})
 
