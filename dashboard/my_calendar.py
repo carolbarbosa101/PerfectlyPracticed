490d45e0b3ad46c7c4ff4f8d21c506addb0bcb1b
Anwin Robin <axr005@student.bham.ac.uk>
2021-07-14 14:17:51 +0100

    Calendar CSS heavily customised (selectors discovered for HTMLCalendar), goals CSS slightly modified

--- dashboard/my_calendar.py
+++ dashboard/my_calendar.py
@@ -1,6 +1,4 @@
 from calendar import HTMLCalendar
 
 class CustomCal(HTMLCalendar):
-    cssclass_month_head = "text-center month-head"
-    cssclass_month = "text-center month"
-    cssclass_year = "text-italic lead"
+    cssclass_month = 'text-center month'
