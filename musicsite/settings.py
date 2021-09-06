9b36f789b2b095f97d716a12921e5592f1cae44a
Anwin Robin <axr005@student.bham.ac.uk>
2021-09-06 16:22:17 +0100

    static dir changed

--- musicsite/settings.py
+++ musicsite/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static_cdn'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
